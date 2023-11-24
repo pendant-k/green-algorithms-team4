30ae2da47ad8a56601e66790bb2e7a95b9553145
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-11-24 22:32:07 +0900

    edited sort vs parallel sort

---
+++ examples/sort-vs-stream-sort-vs-parallel-sort/sort-vs-parallel-sort/Before.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Before {
+
+    private static final int N = 50000000;
+    private static final List<Integer> testData = new ArrayList<>();
+
+    public static List<Integer> init() {
+        Random randomGenerator = new Random();
+        for (int i = 0; i < N; i++) {
+            testData.add(randomGenerator.nextInt(Integer.MAX_VALUE));
+        }
+        return testData;
+    }
+
+    public static void classicSort() {
+
+        Collections.sort(testData);
+    }
+
+    public static void main(String[] args) {
+
+        init();
+        classicSort();
+    }
+}
---
+++ examples/sort-vs-stream-sort-vs-parallel-sort/sort-vs-stream-sort/After.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class After {
+
+    private static final int N = 50000000;
+    private static final List<Integer> testData = new ArrayList<>();
+
+    public static List<Integer> init() {
+        Random randomGenerator = new Random();
+        for (int i = 0; i < N; i++) {
+            testData.add(randomGenerator.nextInt(Integer.MAX_VALUE));
+        }
+        return testData;
+    }
+
+    public static void streamSort() {
+
+        testData.stream().sorted().collect(Collectors.toList());
+    }
+
+
+    public static void main(String[] args) {
+
+        init();
+        streamSort();
+    }
+}
