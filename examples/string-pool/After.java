46add9ba20c45522f159c0c6d1bcc92f7699728c
kyun <annakky@naver.com>
2023-11-26 04:07:13 +0900

    fix: string pool

--- examples/string-pool/After.java
+++ examples/string-pool/After.java
@@ -1,7 +1,7 @@
 public class After {
   public static void main(String[] args) {
-    for(int i = 0; i < 1000000; i++) {
-      String a = "string";
+    for(int i = 0; i < 10000000; i++) {
+      String a = "abcdefghijklmnopqrstuvwxyz01234567890";
     }
   }
 }
--- examples/string-pool/Before.java
+++ examples/string-pool/Before.java
@@ -1,7 +1,7 @@
 public class Before {
   public static void main(String[] args) {
-    for(int i = 0; i < 1000000; i++) {
-      String a = new String("string");
+    for(int i = 0; i < 10000000; i++) {
+      String a = new String("abcdefghijklmnopqrstuvwxyz01234567890");
     }
   }
 }
