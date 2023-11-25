5e7ce93e6fc28da04702932e86a5e85c1dd4a9b1
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-11-26 01:55:25 +0900

    fix linked list vs array deque

--- examples/linked-list-vs-array-deque/After.java
+++ examples/linked-list-vs-array-deque/After.java
@@ -5,11 +5,11 @@ public class After {
     public static void main(String[] args) {
         Deque<Integer> arrayDeque = new ArrayDeque<>();
 
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             arrayDeque.addLast(i);
         }
 
-        for (int i = 0; i < 50000; i++) {
+        for (int i = 0; i < 500000; i++) {
             arrayDeque.addFirst(i);
             arrayDeque.removeLast();
         }
--- examples/linked-list-vs-array-deque/Before.java
+++ examples/linked-list-vs-array-deque/Before.java
@@ -5,11 +5,11 @@ public class Before {
     public static void main(String[] args) {
         List<Integer> linkedList = new LinkedList<>();
 
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             linkedList.add(i);
         }
 
-        for (int i = 0; i < 50000; i++) {
+        for (int i = 0; i < 500000; i++) {
             linkedList.add(0, i);
             linkedList.remove(linkedList.size() - 1);
         }
