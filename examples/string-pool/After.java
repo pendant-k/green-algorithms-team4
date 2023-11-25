009cd2982c8beeb8033b0a87a1ccea56dad6a67b
kyun <annakky@naver.com>
2023-11-26 04:17:29 +0900

    fix: string-pool size up

--- examples/string-pool/After.java
+++ examples/string-pool/After.java
@@ -1,6 +1,6 @@
 public class After {
   public static void main(String[] args) {
-    int dataSize = 1000000;
+    int dataSize = 5000000;
     String[] literalStringArray = new String[dataSize];
 
     for (int i = 0; i < dataSize; i++) {
--- examples/string-pool/Before.java
+++ examples/string-pool/Before.java
@@ -1,8 +1,8 @@
 public class Before {
   public static void main(String[] args) {
-    int dataSize = 1000000;
+    int dataSize = 5000000;
     String[] stringObjectArray = new String[dataSize];
-    
+
     for (int i = 0; i < dataSize; i++) {
       stringObjectArray[i] = new String("value");
     }
