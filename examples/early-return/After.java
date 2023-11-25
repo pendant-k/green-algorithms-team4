3cd5a61eb4e5aa35e00492ff412239bd44ccfc11
kyun <annakky@naver.com>
2023-11-26 01:35:30 +0900

    fix: early return

--- examples/early-return/After.java
+++ examples/early-return/After.java
@@ -1,16 +1,16 @@
 public class After {
   public static int function(int a) {
-    if (a < 10000) return a;
+    if (a < 8000) return a;
 
     int result = 0;
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 1000; i++) {
       result += i;
     }
     return result;
   }
 
   public static void main(String[] args) {
-    for(int i = 0; i < 100000; i++) {
+    for(int i = 0; i < 10000; i++) {
       function(i);
     }
   }
--- examples/early-return/Before.java
+++ examples/early-return/Before.java
@@ -1,16 +1,16 @@
 public class Before {
   public static int function(int a) {
     int result = 0;
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 1000; i++) {
       result += i;
     }
 
-    if (a < 10000) return a;
+    if (a < 8000) return a;
     return result;
   }
 
   public static void main(String[] args) {
-    for(int i = 0; i < 100000; i++) {
+    for(int i = 0; i < 10000; i++) {
       function(i);
     }
   }
