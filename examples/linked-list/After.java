8e5392fea0f29e28e8438fcd6b010954f5ca1cd7
kyun <annakky@naver.com>
2023-11-26 01:53:39 +0900

    fix: linked-list

--- examples/linked-list/After.java
+++ examples/linked-list/After.java
@@ -4,7 +4,7 @@ public class After {
   public static void main(String[] args) {
     ArrayList<Integer> list = new ArrayList<Integer>();
 
-    for (int i = 0; i < 100000; i++) {
+    for (int i = 0; i < 10000000; i++) {
       list.add(i);
     }
   }
--- examples/linked-list/Before.java
+++ examples/linked-list/Before.java
@@ -4,7 +4,7 @@ public class Before {
   public static void main(String[] args) {
     LinkedList<Integer> list = new LinkedList<Integer>();
 
-    for (int i = 0; i < 100000; i++) {
+    for (int i = 0; i < 10000000; i++) {
       list.add(i);
     }
   }
