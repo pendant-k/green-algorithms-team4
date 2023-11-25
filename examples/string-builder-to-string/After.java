da400684a87a8bb13ed86a8fbc41fc12132e94ea
kyun <annakky@naver.com>
2023-11-26 04:21:34 +0900

    fix: string-builder-to-string

--- examples/string-builder-to-string/After.java
+++ examples/string-builder-to-string/After.java
@@ -1,7 +1,10 @@
 public class After {
   public static void main(String[] args) {
-    for(int i = 0; i < 1000000; i++) {
-      new String("string");
+    int dataSize = 5000000;
+    String[] stringList = new String[dataSize];
+
+    for (int i = 0; i < dataSize; i++) {
+      stringList[i] = new String("value");
     }
   }
 }
