ec478c68be9669bfaf3f7c56ebcb87b68b1475a7
kyun <annakky@naver.com>
2023-11-26 04:08:05 +0900

    fix: string-pool size up

--- examples/string-pool/After.java
+++ examples/string-pool/After.java
@@ -1,7 +1,7 @@
 public class After {
   public static void main(String[] args) {
-    for(int i = 0; i < 10000000; i++) {
-      String a = "abcdefghijklmnopqrstuvwxyz01234567890";
+    for(int i = 0; i < 100000000; i++) {
+      String a = "abcdefghijklmnopqrstuvwxyz01234567890abcdefghijklmnopqrstuvwxyz01234567890abcdefghijklmnopqrstuvwxyz01234567890";
     }
   }
 }
