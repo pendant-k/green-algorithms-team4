8e8efd8008bbb8c02eff951991e9dc3ce67ad295
kyun <annakky@naver.com>
2023-11-26 04:04:58 +0900

    feat: string pool

--- examples/string-pool/After.java
+++ examples/string-pool/After.java
@@ -1,7 +1,7 @@
-public class Before {
+public class After {
   public static void main(String[] args) {
     for(int i = 0; i < 1000000; i++) {
-      new StringBuilder().append("string");
+      String a = "string";
     }
   }
 }
--- examples/string-pool/Before.java
+++ examples/string-pool/Before.java
@@ -1,7 +1,7 @@
 public class Before {
   public static void main(String[] args) {
     for(int i = 0; i < 1000000; i++) {
-      new StringBuilder().append("string");
+      String a = new String("string");
     }
   }
 }
