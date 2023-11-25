6a889112b819f11a9f0639fb396d30c00febe341
kyun <annakky@naver.com>
2023-11-26 04:30:26 +0900

    refactor: string-build-to-string

--- examples/string-builder-to-string/After.java
+++ examples/string-builder-to-string/After.java
@@ -1,10 +1,8 @@
 public class After {
   public static void main(String[] args) {
     int dataSize = 5000000;
-    // String[] stringList = new String[dataSize];
 
     for (int i = 0; i < dataSize; i++) {
-      // stringList[i] = new String("value");
       new String("value");
     }
   }
--- examples/string-builder-to-string/Before.java
+++ examples/string-builder-to-string/Before.java
@@ -1,10 +1,8 @@
 public class Before {
   public static void main(String[] args) {
     int dataSize = 5000000;
-    // StringBuilder[] stringBuilderList = new StringBuilder[dataSize];
 
     for (int i = 0; i < dataSize; i++) {
-      // stringBuilderList[i] = new StringBuilder().append("value");
       new StringBuilder().append("value");
     }
   }
