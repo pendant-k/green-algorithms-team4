3a03cb6c3230a6888b9f9b595c78a7a271787ecc
EliseCho13 <dbwlspdlqj@naver.com>
2023-11-26 17:10:02 +0900

    fixed BigDecimal

--- examples/big-decimal/After.java
+++ examples/big-decimal/After.java
@@ -1,3 +1,19 @@
+// public class After {
+//     public static double calculateSimpleInterest(double principal, double rate, int years) {
+//         double interest = principal * rate * years;
+//         return interest;
+//     }
+
+//     public static void main(String[] args) {
+//         double principal = 9000000.0;
+//         double rate = 0.05;
+//         int years = 5;
+
+//   //      System.out.println("After optimization:");
+//         double interest = calculateSimpleInterest(principal, rate, years);
+// //        System.out.println("Simple Interest: " + interest);
+//     }
+// }
 public class After {
     public static double calculateSimpleInterest(double principal, double rate, int years) {
         double interest = principal * rate * years;
@@ -9,9 +25,12 @@ public class After {
         double rate = 0.05;
         int years = 5;
 
-  //      System.out.println("After optimization:");
-        double interest = calculateSimpleInterest(principal, rate, years);
-//        System.out.println("Simple Interest: " + interest);
+        double[] largeArray = new double[10000000];
+
+        for (int i = 0; i < largeArray.length; i++) {
+            largeArray[i] = calculateSimpleInterest(principal, rate, years);
+            principal += 1000.0;
+        }
     }
 }
 
--- examples/big-decimal/Before.java
+++ examples/big-decimal/Before.java
@@ -10,9 +10,15 @@ public class Before {
         BigDecimal principal = new BigDecimal("9000000");
         BigDecimal rate = new BigDecimal("0.05");
         int years = 5;
+        BigDecimal[] largeArray = new BigDecimal[10000000];
+
+        for (int i = 0; i < largeArray.length; i++) {
+            largeArray[i] = calculateSimpleInterest(principal, rate, years);
+            principal = principal.add(new BigDecimal("1000.0"));
+        }
 
   //      System.out.println("Before optimization:");
-        BigDecimal interest = calculateSimpleInterest(principal, rate, years);
+        // BigDecimal interest = calculateSimpleInterest(principal, rate, years);
 //        System.out.println("Simple Interest: " + interest);
     }
 }
